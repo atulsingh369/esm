56990fbaac1991c250afd0bea93e3549c982d68f
atulsingh369 <businessatul7684@gmail.com>
2023-07-10 05:37:44 +0530

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,10 +19,10 @@ const Footer = () => {
               भूतपूर्व सैनिक जन कल्याण समिति
             </p>
           </Link>
-				</div>
-				
+        </div>
+
         <div className="flex text-md font-semibold items-center justify-between">
-          <p>Made with 💓</p>
+          <p>Made with ❤️</p>
           <p>-By Atul Singh</p>
         </div>
       </footer>
