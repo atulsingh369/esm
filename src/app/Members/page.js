d77fa816d8e71bc481392c000ce15423bda4e862
atulsingh369 <businessatul7684@gmail.com>
2023-07-25 11:26:46 +0530

    Updated Search Member Functionality

--- src/app/Members/page.js
+++ src/app/Members/page.js
@@ -22,13 +22,15 @@ const Members = () => {
 
 	const userEmail = searchParams.get('email');
 
-	const searchData = () => {
+	const searchData = (e) => {
+		setSearch(e.target.value);
 		setQueryData([]);
 		data.forEach((item) => {
 			if (item.displayName.toUpperCase().includes(search.trim().toUpperCase())) {
 				setQueryData((queryData) => [...queryData, item]);
 			}
 		})
+		search === "" && setQueryData([]);
 	}
 
 	useEffect(() => {
@@ -38,8 +40,6 @@ const Members = () => {
 			});
 		});
 
-		search.trim() == "" && setQueryData([]);
-		searchData();
 
 		return () => {
 			unsubscribe;
@@ -73,24 +73,25 @@ const Members = () => {
 
 					<h1 id="heading1">Our Members</h1>
 
+
 					{/* Search Members */}
 					<div className="group lg:w-2/6 w-10/12">
 						<input
 							className="input text-lg lg:text-xl"
 							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							onKeyDown={(e) => e.key === 'Enter' && searchData()}
+							onChange={(e) => searchData(e)}
 							type="search"
 							placeholder="Search Members by Name" />
-						<svg onClick={searchData} viewBox="0 0 24 24" aria-hidden="true" className="icon cursor-pointer">
+						<svg viewBox="0 0 24 24" aria-hidden="true" className="icon">
 							<g>
 								<path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
 							</g>
 						</svg>
 					</div>
 
+
 					<div className="flex justify-center lg:justify-end lg:mx-32 mb-12 lg:text-2xl text-xl">
-						Total Members : {data.length}
+						Total Members : {queryData.length > 0 ? queryData.length : data.length}
 					</div>
 
 					{queryData.length > 0 ? (
